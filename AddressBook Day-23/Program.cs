8b57179106ffc034b1cc0d2cb1d6a659adc6f648
Milan Biranwar <milanbiranwar06@gmail.com>
2022-05-27 16:56:39 +0530

    Program for Day23 Adrdress Book

--- AddressBook Day-23/Program.cs
+++ AddressBook Day-23/Program.cs
@@ -1,3 +1,26 @@
 ﻿
 // See https://aka.ms/new-console-template for more information
+using Day23AddressBook;
+
 Console.WriteLine("Hello, World!");
+
+AddressBook addressBook = new AddressBook();
+addressBook.AddNewContact();
+addressBook.AddNewContact();
+
+AddressBook.PrintAddressBook();
+Console.WriteLine("Enter the FirstName to Edit");
+string firstName = Console.ReadLine();
+addressBook.EditContact(firstName);
+AddressBook.PrintAddressBook();
+
+
+AddressBook.PrintAddressBook();
+Console.WriteLine("Enter the FirstName to Delete");
+string firstName1 = Console.ReadLine();
+addressBook.DeleteContact(firstName1);
+AddressBook.PrintAddressBook();
+Console.ReadLine();
+
+
+
